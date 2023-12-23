e381e4cb1f85288d86c73a0f32eb1bcaa5023f98
Tony Zhou <tony.zhoujingtao@gmail.com>
2023-12-23 20:04:49 +0800

    Update database.py for Database Connection Reuse

--- database.py
+++ database.py
@@ -1,62 +1,75 @@
 import sqlite3
-from datetime import datetime
 
 
-def create_table():
-    with sqlite3.connect('test.db') as conn:
-        c = conn.cursor()
-        c.execute('''
-            CREATE TABLE IF NOT EXISTS stocks
-            (symbol TEXT PRIMARY KEY, 
-            tested_at DATETIME NOT NULL)
-        ''')
+class DatabaseManager:
+    def __init__(self, db_path='test.db'):
+        self.db_path = db_path
+        self.conn = None
 
+    def __enter__(self):
+        self.conn = sqlite3.connect(self.db_path)
+        return self
 
-def insert_data(symbol, tested_at):
-    with sqlite3.connect('test.db') as conn:
-        c = conn.cursor()
-        c.execute("INSERT INTO stocks VALUES (?, ?)", (symbol, tested_at))
+    def __exit__(self, exc_type, exc_value, traceback):
+        if self.conn:
+            self.conn.close()
 
+    def create_table(self):
+        with self.conn:
+            cursor = self.conn.cursor()
+            cursor.execute('''
+                CREATE TABLE IF NOT EXISTS stocks
+                (symbol TEXT PRIMARY KEY, 
+                tested_at DATETIME NOT NULL)
+            ''')
 
-def update_data(symbol, tested_at):
-    with sqlite3.connect('test.db') as conn:
-        c = conn.cursor()
-        c.execute("UPDATE stocks SET tested_at = ? WHERE symbol = ?", (tested_at, symbol))
+    def insert_data(self, symbol, tested_at):
+        with self.conn:
+            cursor = self.conn.cursor()
+            cursor.execute("INSERT INTO stocks VALUES (?, ?)", (symbol, tested_at))
 
+    def update_data(self, symbol, tested_at):
+        with self.conn:
+            cursor = self.conn.cursor()
+            cursor.execute("UPDATE stocks SET tested_at = ? WHERE symbol = ?", (tested_at, symbol))
 
-def delete_data(symbol):
-    with sqlite3.connect('test.db') as conn:
-        print(f"Deleting {symbol} from database ...")
-        c = conn.cursor()
-        c.execute("DELETE from stocks WHERE symbol = ?", (symbol,))
+    def delete_data(self, symbol):
+        with self.conn:
+            cursor = self.conn.cursor()
+            cursor.execute("DELETE from stocks WHERE symbol = ?", (symbol,))
 
-
-def read_data(symbol=None):
-    with sqlite3.connect('test.db') as conn:
-        c = conn.cursor()
+    def read_data(self, symbol=None):
+        cursor = self.conn.cursor()
+        query = "SELECT * FROM stocks"
         if symbol:
-            c.execute("SELECT * FROM stocks WHERE symbol=?", (symbol,))
+            query += " WHERE symbol = ?"
+            cursor.execute(query, (symbol,))
         else:
-            c.execute("SELECT * FROM stocks")
-        rows = c.fetchall()
-    return rows
+            cursor.execute(query)
+        return cursor.fetchall()
+
+
+def refresh():
+    with open("tickers.txt", "r") as file:
+        with DatabaseManager('test.db') as db:
+            for line in file:
+                symbol = line.strip()
+                db.delete_data(symbol)
+
+
+def test_run():
+    with DatabaseManager('test.db') as db:
+        db.create_table()
+        db.insert_data('AAPL', '2023-12-22')
+        print(db.read_data('AAPL'))
+        db.update_data('AAPL', '2023-12-23')
+        print(db.read_data('AAPL'))
+        db.delete_data('AAPL')
+        print(db.read_data())
 
 
 def main():
-    with open("tickers.txt", "r") as file:
-        for line in file:
-            symbol = line.strip()
-            delete_data(symbol)
-
-
-def simple_test():
-    create_table()
-    insert_data('AAPL', datetime.now())
-    print(read_data('AAPL'))  # Prints: [('AAPL', '2023-06-13')]
-    update_data('AAPL', datetime.now())
-    print(read_data('AAPL'))  # Prints: [('AAPL', '2023-06-14')]
-    delete_data('AAPL')
-    print(read_data())  # Prints: []
+    test_run()
 
 
 if __name__ == '__main__':
